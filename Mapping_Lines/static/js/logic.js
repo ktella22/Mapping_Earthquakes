791b4cfdf1ab788f9e2febb7e19938542c6ebba1
kathynwe <ktnwe22@gmail.com>
2022-01-17 20:03:30 -0800

    update

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,21 +1,21 @@
 // Add console.log to check to see if our code is working
 console.log("working");
 
-// Create a map object with a center and zoom level
-let map = L.map("mapid").setView([37.6213, -122.3790], 5);
+// Create a map object with a center at San Francisco airport and zoom level
+let map = L.map("mapid").setView([37.6213, -122.3709], 5);
 
-// Coordinates for each point to be used in the line
+// Coordinates for each point to be used in the polyline
 let line = [
-    [33.9416, -118.4085],
+    [33.9416, -118,4085],
     [37.6213, -122.3790],
-    [40.7899, -111.9791], 
+    [40.7899, -111.9791],
     [47.4502, -122.3088]
 ];
 
-// Create a polyline using the line coordinates and make the line red. 
+// Create a polyline using the line coordinates and make the line red
 L.polyline(line, {
     color: "yellow"
-}).addTo(map);
+});
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
